e0fda46124c51a95a35bf90b339f4ec730f63f2a
zhangxiaoyu5 <zhangxiaoyu5@yonyou.com>
2026-01-27 15:18:01 +0800

    自定义智能客服 Advisor: 开发 SSE 流式聊天接口

--- src/main/java/com/quanxiaoha/ai/robot/controller/AiCustomerServiceController.java
+++ src/main/java/com/quanxiaoha/ai/robot/controller/AiCustomerServiceController.java
@@ -1,20 +1,11 @@
 package com.quanxiaoha.ai.robot.controller;
 
 import com.google.common.collect.Lists;
-import com.quanxiaoha.ai.robot.advisor.CustomChatMemoryAdvisor;
-import com.quanxiaoha.ai.robot.advisor.CustomStreamLoggerAndMessage2DBAdvisor;
-import com.quanxiaoha.ai.robot.advisor.NetworkSearchAdvisor;
+import com.quanxiaoha.ai.robot.advisor.CustomerServiceAdvisor;
 import com.quanxiaoha.ai.robot.aspect.ApiOperationLog;
-import com.quanxiaoha.ai.robot.domain.mapper.ChatMessageMapper;
-import com.quanxiaoha.ai.robot.model.vo.chat.*;
-import com.quanxiaoha.ai.robot.model.vo.customerService.DeleteMarkdownFileReqVO;
-import com.quanxiaoha.ai.robot.model.vo.customerService.FindMarkdownFilePageListReqVO;
-import com.quanxiaoha.ai.robot.model.vo.customerService.FindMarkdownFilePageListRspVO;
-import com.quanxiaoha.ai.robot.model.vo.customerService.UpdateMarkdownFileReqVO;
-import com.quanxiaoha.ai.robot.service.ChatService;
+import com.quanxiaoha.ai.robot.model.vo.chat.AIResponse;
+import com.quanxiaoha.ai.robot.model.vo.customerService.*;
 import com.quanxiaoha.ai.robot.service.CustomerService;
-import com.quanxiaoha.ai.robot.service.SearXNGService;
-import com.quanxiaoha.ai.robot.service.SearchResultContentFetcherService;
 import com.quanxiaoha.ai.robot.utils.PageResponse;
 import com.quanxiaoha.ai.robot.utils.Response;
 import jakarta.annotation.Resource;
@@ -25,16 +16,15 @@ import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.ai.openai.api.OpenAiApi;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
-import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import reactor.core.publisher.Flux;
 
 import java.util.List;
-
 
 /**
  * @Author: 犬小哈
@@ -49,10 +39,20 @@ public class AiCustomerServiceController {
 
     @Resource
     private CustomerService customerService;
+    @Resource
+    private VectorStore vectorStore;
+
+    @Value("${spring.ai.openai.base-url}")
+    private String baseUrl;
+    @Value("${spring.ai.openai.api-key}")
+    private String apiKey;
+    @Value("${customer-service.model}")
+    private String model;
+    @Value("${customer-service.temperature}")
+    private Double temperature;
 
     /**
      * 问答 MD 文件上传
-     *
      * @param file
      * @return
      */
@@ -66,6 +66,7 @@ public class AiCustomerServiceController {
     public Response<?> deleteMarkdownFile(@RequestBody @Validated DeleteMarkdownFileReqVO deleteMarkdownFileReqVO) {
         return customerService.deleteMarkdownFile(deleteMarkdownFileReqVO);
     }
+
     @PostMapping("/md/list")
     @ApiOperationLog(description = "Markdown 问答文件分页查询")
     public PageResponse<FindMarkdownFilePageListRspVO> findMarkdownFilePageList(@RequestBody @Validated FindMarkdownFilePageListReqVO findMarkdownFilePageListReqVO) {
@@ -73,10 +74,48 @@ public class AiCustomerServiceController {
     }
 
     @PostMapping("/md/update")
-    @ApiOperationLog(description = "修改 Markdown 问答文件信息")
+    @ApiOperationLog(description = "删除 Markdown 问答文件")
     public Response<?> updateMarkdownFile(@RequestBody @Validated UpdateMarkdownFileReqVO updateMarkdownFileReqVO) {
         return customerService.updateMarkdownFile(updateMarkdownFileReqVO);
     }
 
+    /**
+     * 流式对话
+     * @return
+     */
+        @PostMapping(value = "/completion", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @ApiOperationLog(description = "AI 智能客服对话")
+        public Flux<AIResponse> chat(@RequestBody @Validated AiCustomerServiceChatReqVO chatReqVO) {
+        String userMessage = chatReqVO.getMessage();
+        // 构建 ChatModel
+        ChatModel chatModel = OpenAiChatModel.builder()
+                .openAiApi(OpenAiApi.builder()
+                        .baseUrl(baseUrl)
+                        .apiKey(apiKey)
+                        .build())
+                .build();
+
+        // 动态设置调用的模型名称、温度值
+        ChatClient.ChatClientRequestSpec chatClientRequestSpec = ChatClient.create(chatModel)
+                .prompt()
+                .options(OpenAiChatOptions.builder()
+                        .model(model)
+                        .temperature(temperature)
+                        .build())
+                .user(userMessage); // 用户提示词
+
+        // Advisor 集合
+        List<Advisor> advisors = Lists.newArrayList();
+        advisors.add(new CustomerServiceAdvisor(vectorStore)); // 检索向量库，组合增强提示词
+
+        // 应用 Advisor 集合
+        chatClientRequestSpec.advisors(advisors);
+
+        // 流式输出
+       return chatClientRequestSpec
+                .stream()
+                .content()
+                .mapNotNull(text -> AIResponse.builder().v(text).build()); // 构建返参 AIResponse
+    }
 
 }
